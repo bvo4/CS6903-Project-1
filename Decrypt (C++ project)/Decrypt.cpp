aa0d3364c1e162db5d977f80d0e384df3a054cfc
bvo4 <brandonvo1980@yahoo.com>
2022-02-06 14:11:47 -0500

    Adding pseudocode

--- Decrypt (C++ project)/Decrypt.cpp
+++ Decrypt (C++ project)/Decrypt.cpp
@@ -1,9 +1,27 @@
 #include<iostream>
 #include<string>
+#include<random>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
 
 using namespace std;
 
+char random_letter_generator()
+{
+	return 'a' + rand() % 26;
+}
+
+int coin_generation_algorithm(int ciphertext_pointer, L)
+{
+	int coin_value = 0;
+	return coin_value;
+}
+
 /*
+
+Pseudocode
 
 Instructions:
 ciphertext_pointer = 1
@@ -26,22 +44,51 @@ Return c[1]...c[L + num_rand_characters]
 
 */
 
-/*
-print on screen something like "Enter the ciphertext:",
-obtain the ciphertext from stdin,
-apply some cryptanalysis strategy and
-output on screen something like "My plaintext guess is:" followed by the plaintext found by your strategy.
-*/
+//Do decryption here
+string decryption_scheme(string input) {
+	string output;
+	int j = 0;
 
-void decryption_scheme(string input) {
 
+	int ciphertext_pointer = 1;
+	int message_pointer = 1;
+	int num_rand_characters = 0;
+	int prob_of_random_ciphertext = 0.95;
+	//Repeat
+	int coin_value = coin_generation_algorithm(ciphertext_pointer, L);  // coin_value is a real number in [0,1]
+	if (prob_of_random_ciphertext < coin_value && prob_of_random_ciphertext <= 1)
+	{
+		j = m[message_pointer]; // j is a value between 0 and 26
+		c[ciphertext_pointer] = k[j];
+		message_pointer++;
+	}
+
+	if (0 <= coin_value && coin_value <= prob_of_random_ciphertext)
+	{
+
+		//randomly choose a character c from{ <space>,a,..,z }
+		char rand_letter = random_letter_generator();
+
+
+		set c[ciphertext_pointer] = c
+		num_rand_characters = num_rand_characters + 1
+		ciphertext_pointer = ciphertext_pointer + 1
+		Until ciphertext_pointer > L + num_rand_characters
+	}
+		Return c[1]...c[L + num_rand_characters]
+
+	return output;
 }
 
 int main() {
 
+//1. print on screen something like "Enter the ciphertext:",
+//2. obtain the ciphertext from stdin,
 	string input;
-
+	string output;
 	cout << "Enter the ciphertext: " << endl;
-	cin >> input
+	cin >> input;
 
+	output = decryption_scheme(input);
+	cout << "My plaintext guess is:  " << output << endl;
 }
