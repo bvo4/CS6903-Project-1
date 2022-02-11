d0fd88d6cd6b06d14345638ba616f096dccab291
bvo4 <brandonvo1980@yahoo.com>
2022-02-11 08:52:28 -0500

    Using GeeksforGeeks encryption for now

--- Decrypt (C++ project)/Decrypt.cpp
+++ Decrypt (C++ project)/Decrypt.cpp
@@ -10,7 +10,8 @@
 
 using namespace std;
 
-string encrypt(string input);
+string encrypt(string input, string key);
+string cipheredIt(string msg, string encoded);
 
 //randomly choose a character c from{ <space>,a,..,z }
 char random_letter_generator()
@@ -18,7 +19,8 @@ char random_letter_generator()
 	return 'a' + rand() % 26;
 }
 
-int coin_generation_algorithm(int ciphertext_pointer, L)
+
+int coin_generation_algorithm(int ciphertext_pointer, int L)
 {
 	// coin_value is a real number in [0,1]
 	int coin_value = 0;
@@ -52,23 +54,25 @@ Return c[1]...c[L + num_rand_characters]
 */
 
 //Do decryption here
-string decryption_scheme(string input) {
+string decryption_scheme(string input, string k) {
 	/* The different parameters */
-	int L = 500;
-	int u = 5;
+	int L = 500;		//Length of message
+	int u = 5;			
 	int v = 50;
 	int t = 5;		//Between 1 and 24
 
 	string output;
 	int j = 0;
 
-
+	
 	int ciphertext_pointer = 1;
 	int message_pointer = 1;
 	int num_rand_characters = 0;
 	int prob_of_random_ciphertext = 0.95;
 	//Repeat
+	
 	int coin_value = coin_generation_algorithm(ciphertext_pointer, L);  // coin_value is a real number in [0,1]
+	/*
 	if (prob_of_random_ciphertext < coin_value && prob_of_random_ciphertext <= 1)
 	{
 		j = m[message_pointer]; // j is a value between 0 and 26
@@ -87,6 +91,7 @@ string decryption_scheme(string input) {
 		Until ciphertext_pointer > L + num_rand_characters
 	}
 	//Return c[1]...c[L + num_rand_characters]
+	*/
 	return output;
 
 }
@@ -95,34 +100,113 @@ int main() {
 
 	string input;
 	string output;
+	string key
 
 	if (ENCRYPT)
 	{
+		//Uses a mono-alphabetic substitution cipher and attempts to decrypt it
+
 		cout << "Enter plaintext " << endl;
 		cin >> input;
-		cout << "Ciphertext:  " << endl;
+
+		cout << "Enter the key:  " << endl;
+		cin >> key;
+
+		key = "secret";
+		key = encrypt(input, key);
+
+		output = cipheredIt(input, key);
+
+		cout << "Ciphertext:  " << output << endl;
+
+		output = decryption_scheme(input, key);
 	}
 	else
 	{
+
+		//Input would be key k=k[0],...,k[26] and message m=m[1],...,m[L]
 		cout << "Enter the ciphertext: " << endl;
 		cin >> input;
 
-		output = decryption_scheme(input);
+		output = decryption_scheme(input, key);
 		cout << "My plaintext guess is:  " << output << endl;
 	}
 }
 
-string encrypt(string input)
+//Taken from:  https://www.geeksforgeeks.org/keyword-cipher/#:~:text=A%20keyword%20cipher%20is%20a,%2C%20B%2C%20C%2C%20etc.
+//Will remove once we get the plaintext dictionary
+string encrypt(string input, string key)
 {
 	/*
 	Professor mentioned that given a key,
 	we would be able to encrypt a plaintesxt message into the matching ciphertext,
 	so leaving this here in case it's needed
 	*/
-	
-	string ciphertext;
-	//Defines which of the 26 letters are being used
-	bool letters[26];
+	string encoded = "";
+	// This array represents the
+	// 26 letters of alphabets
+	bool arr[26] = { 0 };
 
+	// This loop inserts the keyword
+	// at the start of the encoded string
+	for (int i = 0; i < key.size(); i++)
+	{
+		if (key[i] >= 'A' && key[i] <= 'Z')
+		{
+			// To check whether the character is inserted
+			// earlier in the encoded string or not
+			if (arr[key[i] - 65] == 0)
+			{
+				encoded += key[i];
+				arr[key[i] - 65] = 1;
+			}
+		}
+		else if (key[i] >= 'a' && key[i] <= 'z')
+		{
+			if (arr[key[i] - 97] == 0)
+			{
+				encoded += key[i] - 32;
+				arr[key[i] - 97] = 1;
+			}
+		}
+	}
 
+	// This loop inserts the remaining
+	// characters in the encoded string.
+	for (int i = 0; i < 26; i++)
+	{
+		if (arr[i] == 0)
+		{
+			arr[i] = 1;
+			encoded += char(i + 65);
+		}
+	}
+	return encoded;
+}
+
+// Function that generates encodes(cipher) the message
+string cipheredIt(string msg, string encoded)
+{
+	string cipher = "";
+
+	// This loop ciphered the message.
+	// Spaces, special characters and numbers remain same.
+	for (int i = 0; i < msg.size(); i++)
+	{
+		if (msg[i] >= 'a' && msg[i] <= 'z')
+		{
+			int pos = msg[i] - 97;
+			cipher += encoded[pos];
+		}
+		else if (msg[i] >= 'A' && msg[i] <= 'Z')
+		{
+			int pos = msg[i] - 65;
+			cipher += encoded[pos];
+		}
+		else
+		{
+			cipher += msg[i];
+		}
+	}
+	return cipher;
 }
