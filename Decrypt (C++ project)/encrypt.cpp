f9f4991885bdcefad993de8955f9b6ba764030bd
bvo4 <brandonvo1980@yahoo.com>
2022-02-27 14:41:09 -0500

    An attempt at refactoring, but leads to linker errors.  Will be adding this and merging with the other branch before reverting this change.

---
+++ Decrypt (C++ project)/encrypt.cpp
@@ -0,0 +1,83 @@
+#include "encrypt.h"
+
+using namespace std;
+
+
+// coin_value is a real number in [0,1]
+int encryption::coin_generation_algorithm(int ciphertext_pointer, int L)
+{
+	// Initialize Mersenne Twister pseudo-random number generator
+	random_device rd;
+	mt19937 gen(rd());
+
+	uniform_int_distribution<> dis(0, 1);
+	return dis(gen);
+}
+
+//randomly choose a character c from{ <space>,a,..,z }
+char encryption::random_letter_generator()
+{
+	// Initialize Mersenne Twister pseudo-random number generator
+	random_device rd;
+	mt19937 gen(rd());
+
+	//Choose a random letter a-z
+	uniform_int_distribution<> dis(0, 26);
+	int random_number = dis(gen);
+	if (random_number >= 26)
+		return ' ';
+
+	return char('a' + random_number);
+}
+string encryption::encrypt(std::string input, std::string key)
+{
+	int ciphertext_pointer = 0;
+	int message_pointer = 0;
+	int num_rand_characters = 0;
+	int prob_of_random_ciphertext = 0;
+	char CT[500];
+
+	int coin_value = coin_generation_algorithm(ciphertext_pointer, input.length());  // coin_value is a real number in [0,1]
+
+/*
+TA NOTE:
+	abc  = plain text
+	abc = KEY
+	pt pointer is at a
+	flip a coin. if 0<=value<=prob of random ciphertextncy
+	insert random char
+	else
+	encrypt pt a with char a from the key
+*/
+	do {
+		//cout << "input #" << message_pointer << " : " << input[message_pointer] << endl;
+
+
+		if (prob_of_random_ciphertext < coin_value && coin_value <= 1)
+		{
+			//set j = m[message_pointer] // j is a value between 0 and 26
+			int j = input[message_pointer];
+
+			//set c[ciphertext_pointer] = k[j]
+			CT[ciphertext_pointer] = key[j % key.length()];
+
+			message_pointer++;
+			ciphertext_pointer++;
+		}
+
+		else if (0 <= coin_value && coin_value <= prob_of_random_ciphertext)
+		{
+			//randomly choose a character c from {<space>,a,..,z}
+			char c = random_letter_generator();
+			//set c[ciphertext_pointer] = c
+			CT[ciphertext_pointer] = c;
+			ciphertext_pointer++;
+		}
+		//Until ciphertext_pointer > L + num_rand_characters
+	} while (ciphertext_pointer < (input.length() - 1 + num_rand_characters) && message_pointer < 500);
+	//Return c[1]...c[L + num_rand_characters]
+
+	//Forcibly turncating to length 500 for now
+	//NEED TO REMOVE THIS LATER WHEN ACTUAL PRACTICE IS USED
+	return std::string(CT).substr(0, 499);
+}
